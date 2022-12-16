d3e100fe7e5e1685f2839cdb246bc328e5570644
luo00010 <1851298930@qq.com>
2022-12-16 13:08:18 +0800

    TreeMap测试比较类

---
+++ Day24/src/com/luo/java1/Employee.java
@@ -0,0 +1,61 @@
+package com.luo.java1;
+
+
+import java.util.Objects;
+
+class Employee implements Comparable{
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return age == employee.age && Objects.equals(name, employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (o instanceof Employee){
+            Employee e = (Employee) o;
+            return this.name.compareTo(e.name);
+        } throw new RuntimeException("类型不一致");
+    }
+}
