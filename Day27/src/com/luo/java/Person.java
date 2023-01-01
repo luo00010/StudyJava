6c556496e8ca3d6b734b4ebb0d1bf59e81298b3a
luo00010 <1851298930@qq.com>
2023-01-01 22:23:53 +0800

    可序列化的类Test

---
+++ Day27/src/com/luo/java/Person.java
@@ -0,0 +1,44 @@
+package com.luo.java;
+
+import java.io.Serializable;
+
+/**
+ * @author luozstart
+ * @create 2023-01-01 19:58
+ */
+public class Person implements Serializable {
+
+    public static final long serialVersionUID = 21355L;
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
